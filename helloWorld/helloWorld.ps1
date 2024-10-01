a66bd1b6bb65daa3631306e3c9c12d232e7308a9
Fredray-21 <dabadie.frederic@gmail.com>
2024-10-01 20:11:55 +0200

    WIP(helloWorld) : style un ajoute un crédit car pk pas

--- helloWorld/helloWorld.ps1
+++ helloWorld/helloWorld.ps1
@@ -14,9 +14,24 @@ $Label.AutoSize = $true
 $Label.TextAlign = 'MiddleCenter'
 $Label.ForeColor = 'Red'
 
+$LabelCredit = New-Object -TypeName System.Windows.Forms.LinkLabel
+$LabelCredit.Font = New-Object System.Drawing.Font("Arial", 12, [System.Drawing.FontStyle]::Regular)
+$LabelCredit.Text = 'Par : @Fredray-21'
+$LabelCredit.AutoSize = $true
+$LabelCredit.TextAlign = 'MiddleCenter'
+$LabelCredit.ForeColor = 'Black'
+$LabelCredit.Links.Add(6, 12, 'https://github.com/Fredray-21')
+$LabelCredit.add_LinkClicked({
+    Start-Process $_.Link.LinkData
+})
+
+$form.Controls.Add($LabelCredit)
 $form.Controls.Add($Label)
 
 $Label.Left = ($form.ClientSize.Width - $Label.Width) / 2
 $Label.Top = ($form.ClientSize.Height - $Label.Height) / 2
+
+$LabelCredit.Left = ($form.ClientSize.Width - $LabelCredit.Width) / 2
+$LabelCredit.Top = $Label.Top + $Label.Height + 10
 
 $form.ShowDialog()
